787ccf3ffb58e1188e7ec6a0df8ea2c49ff6fa86
toran billups <toranb@gmail.com>
2012-07-28 19:45:26 -0500

    added support for express 3.0

--- cookbooks/main/recipes/express.rb
+++ cookbooks/main/recipes/express.rb
@@ -24,6 +24,33 @@ end
 
 execute "cd /etc/node && npm install"
 execute "cd /var/log && touch node.log && chmod 765 node.log && chown -R root:www-data node.log"
+
+node[:node_apps].each do |app|
+    directory "/etc/node/#{app[:name]}/" do
+        owner 'root'
+        group 'www-data'
+        mode 0755
+        recursive true
+    end
+
+    cookbook_file "/etc/node/#{app[:name]}/package.json" do
+        source "package.json"
+    end
+
+    execute "cd /etc/node/#{app[:name]} && npm install"
+    execute "cd /etc/node/#{app[:name]} && touch access.log && chmod 765 access.log && chown -R root:www-data access.log"
+
+    template "jsfile" do
+        path "/etc/node/#{app[:name]}/app.js"
+        source "app.erb"
+        variables :name => app[:name]
+        owner "root"
+        group "www-data"
+        mode 0755
+    end
+
+end
+
 execute "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000"
 execute "iptables-save > /etc/iptables.rules"
 
